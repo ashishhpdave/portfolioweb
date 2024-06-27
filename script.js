b1da57cfaa32500f46c63c3372dfd9071bf49dd0
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-06-27 12:06:19 +0530

    Animastion-Add
    This is a aniamstion and a little bit use of js

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".auto-type",{
+    strings : ["Programer", "Designer", "Frouend Developer" , "Backend Developer", "MERN Develper"],
+    typeSpeed : 160,
+    backSpeed : 150,
+    loop : true
+});     
+                    
