039c83a1eee41df448ccc607ab4d03c09cd2f726
ashishhpdave <141114347+ashishhpdave@users.noreply.github.com>
2024-07-02 22:18:55 +0530

    border-radious

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type",{
-    strings : ["Programer", "Designer", "Frouend Developer" , "Backend Developer", "MERN Develper"],
+    strings : ["Programer", "Designer", "Frontend Developer" , "Backend Developer", "MERN Developer"],
     typeSpeed : 160,
     backSpeed : 150,
     loop : true
